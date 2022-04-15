881c5a0dccbbdcceb3a517c7d0173cdd42846bdf
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-04-15 11:04:10 +0100

    Added Assembly Resolver

--- Main.cs
+++ Main.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using NavisDataExtraction.Wpf.Views;
 using NavisDataExtraction.Wpf.ViewModels;
 using NavisDataExtraction.Wpf;
@@ -19,13 +21,29 @@ namespace NavisDataExtraction
         DisplayName ="Export")]
     public class Main : AddInPlugin
     {
+        private static string _thisAssemblyPath;
         public override int Execute(params string[] parameters)
         {
+            _thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblies;
+            
             var window = new MainWindow();
-            ///ElementHost.EnableModelessKeyboardInterop(window);
-            window.Show();
             ElementHost.EnableModelessKeyboardInterop(window);
+            window.Show();
             return 0;
-        }       
+        }
+
+        private Assembly ResolveAssemblies(object sender, ResolveEventArgs args)
+        {
+            
+            var path = Path.GetDirectoryName(_thisAssemblyPath);
+            if (path == null) return null;
+            var dll = $"{new Regex(",.*").Replace(args.Name, string.Empty)}.dll";
+            var file = Path.Combine(path, dll);
+
+            if (!File.Exists(file)) return null;
+
+            return Assembly.LoadFrom(file);
+        }
     }
 }
