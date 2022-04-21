a6ddfe39f0c579a0def29a784416b4bc3fdeb821
Piotr Kulicki <piotr.kulicki@gmail.com>
2022-04-21 19:56:16 +0100

    Basic Setup for new .NET SDK Project

---
+++ PM.Navisworks.DataExtraction/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace PM.Navisworks.DataExtraction
+{
+    [Plugin("NavisDataExtraction.DataExport",
+        "PMPK",
+        ToolTip = "Export Data to Json",
+        DisplayName ="DataExtraction")]
+    [RibbonTab("PMG_CustomTab_01", DisplayName = "PMG Tools")]
+    [Command("ID_BUtton_01",ToolTip ="Interface")]
+    public class Main : AddInPlugin
+    {
+        private static string _thisAssemblyPath;
+        public override int Execute(params string[] parameters)
+        {
+            _thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssemblies;
+            
+            // var window = new MainWindow();
+            // ElementHost.EnableModelessKeyboardInterop(window);
+            // window.Show();
+            return 0;
+        }
+
+        private Assembly ResolveAssemblies(object sender, ResolveEventArgs args)
+        {
+            
+            var path = Path.GetDirectoryName(_thisAssemblyPath);
+            if (path == null) return null;
+            var dll = $"{new Regex(",.*").Replace(args.Name, string.Empty)}.dll";
+            var file = Path.Combine(path, dll);
+
+            if (!File.Exists(file)) return null;
+
+            return Assembly.LoadFrom(file);
+        }
+    }
+}
